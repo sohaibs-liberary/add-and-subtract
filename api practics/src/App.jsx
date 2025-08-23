8507d628b90bb6ac24ca271de3a68b8ae343342d
sohaibs-liberary <muhammadsohaibshakeel8gmail.com>
2025-08-24 01:05:49 +0500

    shop api intigrated

--- api practics/src/App.jsx
+++ api practics/src/App.jsx
@@ -1,19 +1,45 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((responsive) => responsive.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error("Error in  API Fetching", error));
+  }, []);
 
   return (
-    <div>
+    <>
+      <div className="body">
+        <div>
+          <h1 className="name">Store</h1>
+          <p className="about">
+            We have all kinds of Men online shopping variety available, from
+            Men's Kurta Online Shopping, Men's shoes online shopping, Men's
+            clothing, accessories and much
+          </p>
+          {data.map((item, index) => (
+            <div key={index}>
+              <div className="cards">
+                 <div>
 
-      <h1>click for add</h1>
-      <button onClick={()=> setCount(count + 1)} >  + </button>
-      <p>{count}</p>
-     <h1>Click for subtract</h1>
-      <button onClick={()=> setCount(count - 1)} > - </button>
-      <p>{count}</p>
-    
-    </div>
+                  <img
+                    src={item.images[0]}
+                    alt="product image"
+                    className="productimg"
+                    />
+                  <h1>{item.title}</h1>
+                  <p>{item.description}</p>
+                    </div>
+                    
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
 
